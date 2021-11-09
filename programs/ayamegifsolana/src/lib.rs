7cfdd59517c59dd128347f00feb6e2e2f1022d62
ElvisYong <elvisyong@gmail.com>
2021-11-10 01:52:01 +0800

    Change program id onto deployed devnet id

--- programs/ayamegifsolana/src/lib.rs
+++ programs/ayamegifsolana/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("7fFqkK9ScNdwZnGoVhCBjc1zMynoiND95mcQWqr8tamR");
 
 #[program]
 pub mod ayamegifsolana {
